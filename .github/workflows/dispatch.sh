c151df233cc80b69341de34534fec3ce6df22333
染矢馨子 <>
2019-11-25 23:40:21 +0900

    refactor: 不要なファイルを削除

--- .github/workflows/dispatch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -X POST -H "Authorization: token $MY_TOKEN" -H "Accept: application/vnd.github.everest-preview+json" --data '{"event_type": "test-hello"}' https://api.github.com/repos/yykaoruko/rukolog/dispatches
